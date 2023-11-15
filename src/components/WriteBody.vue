c15b42b648781ba4f8959090bf4ce219a6802d76
hgleeee <94894328+hgleeee@users.noreply.github.com>
2023-11-15 17:26:11 +0900

    [#3] feat: 글 작성 페이지 구성

---
+++ src/components/WriteBody.vue
@@ -0,0 +1,180 @@
+<script lang="ts" setup>
+import StarterKit from '@tiptap/starter-kit'
+import type { UploadFile } from 'element-plus'
+import { ref, computed, watch, onMounted } from 'vue'
+import type { Ref } from 'vue' 
+import Image from '@tiptap/extension-image'
+import { Editor, EditorContent } from '@tiptap/vue-3'
+
+
+const emit = defineEmits(['updateEditor', 'postAttachImage']);
+const props = defineProps(['image', 'content']);
+
+const editor = new Editor({
+    extensions: [
+        StarterKit,
+        Image,
+    ],
+    content: props.content,
+    onUpdate() {
+        emit('updateEditor', editor.getHTML())
+    }
+})
+
+const toAttachImage: Ref<any> = computed(() => {
+    return props.image;
+})
+
+watch(toAttachImage, () => {
+    if (toAttachImage.value === null) return;
+    console.log(toAttachImage.value.url);
+    editor.chain().focus().setImage({ src: toAttachImage.value.url }).run();
+    emit('postAttachImage');
+})
+
+// onMounted(() => {
+//     editor.chain().focus().setTextSelection(10).run();
+// })
+
+</script>
+
+<template>
+<div class="menu-bar" v-if="editor">
+    <button type="button" @click="editor.chain().focus().toggleBold().run()" :disabled="!editor.can().chain().focus().toggleBold().run()" :class="{ 'is-active': editor.isActive('bold') }">
+        bold
+    </button>
+    <button type="button" @click="editor.chain().focus().toggleItalic().run()" :disabled="!editor.can().chain().focus().toggleItalic().run()" :class="{ 'is-active': editor.isActive('italic') }">
+        italic
+    </button>
+    <button type="button" @click="editor.chain().focus().toggleStrike().run()" :disabled="!editor.can().chain().focus().toggleStrike().run()" :class="{ 'is-active': editor.isActive('strike') }">
+        strike
+    </button>
+    <button type="button" @click="editor.chain().focus().toggleCode().run()" :disabled="!editor.can().chain().focus().toggleCode().run()" :class="{ 'is-active': editor.isActive('code') }">
+        code
+    </button>
+    <button type="button" @click="editor.chain().focus().setParagraph().run()" :class="{ 'is-active': editor.isActive('paragraph') }">
+        paragraph
+    </button>
+    <button type="button" @click="editor.chain().focus().toggleHeading({ level: 1 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }">
+        h1
+    </button>
+    <button type="button" @click="editor.chain().focus().toggleHeading({ level: 2 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }">
+        h2
+    </button>
+    <button type="button" @click="editor.chain().focus().toggleHeading({ level: 3 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }">
+        h3
+    </button>
+    <button type="button" @click="editor.chain().focus().toggleHeading({ level: 4 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }">
+        h4
+    </button>
+    <button type="button" @click="editor.chain().focus().toggleHeading({ level: 5 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 5 }) }">
+        h5
+    </button>
+    <button type="button" @click="editor.chain().focus().toggleBulletList().run()" :class="{ 'is-active': editor.isActive('bulletList') }">
+        bullet list
+    </button>
+    <button type="button" @click="editor.chain().focus().toggleOrderedList().run()" :class="{ 'is-active': editor.isActive('orderedList') }">
+        ordered list
+    </button>
+    <button type="button" @click="editor.chain().focus().toggleCodeBlock().run()" :class="{ 'is-active': editor.isActive('codeBlock') }">
+        code block
+    </button>
+    <button type="button" @click="editor.chain().focus().toggleBlockquote().run()" :class="{ 'is-active': editor.isActive('blockquote') }">
+        blockquote
+    </button>
+    <button type="button" @click="editor.chain().focus().undo().run()" :disabled="!editor.can().chain().focus().undo().run()">
+        undo
+    </button>
+    <button type="button" @click="editor.chain().focus().redo().run()" :disabled="!editor.can().chain().focus().redo().run()">
+        redo
+    </button>
+</div>
+<div>
+    <editor-content :editor="editor" />
+</div>
+
+</template>
+
+  
+<style lang="scss">
+.menu-bar {
+    padding-bottom: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    button {
+        font-size: 15px;
+        outline: none;
+        border: none;
+        background: transparent;
+        color: rgb(70, 70, 70);
+        cursor: pointer;
+    }
+    button.is-active {
+        background: rgb(197, 197, 197);
+        padding: 2px 3px;
+        border-radius: 2px;
+    }
+}
+
+/* Basic editor styles */
+.tiptap {
+    min-height: 350px;
+    padding: 5px;
+
+    > * + * {
+        margin-top: 0.75em;
+    }
+
+    ul,
+    ol {
+        padding: 0 1rem;
+    }
+
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+        line-height: 1.1;
+    }
+
+    code {
+        background-color: rgba(#616161, 0.1);
+        color: #616161;
+    }
+
+    pre {
+        background: #0D0D0D;
+        color: #FFF;
+        font-family: 'JetBrainsMono', monospace;
+        padding: 0.75rem 1rem;
+        border-radius: 0.5rem;
+
+        code {
+        color: inherit;
+        padding: 0;
+        background: none;
+        font-size: 0.8rem;
+        }
+    }
+
+    img {
+        max-width: 100%;
+        height: auto;
+    }
+
+    blockquote {
+        padding-left: 1rem;
+        border-left: 2px solid rgba(#0D0D0D, 0.1);
+    }
+
+    hr {
+        border: none;
+        border-top: 2px solid rgba(#0D0D0D, 0.1);
+        margin: 2rem 0;
+    }
+}
+</style>
--- src/views/WriteView.vue
+++ src/views/WriteView.vue
@@ -1,31 +1,81 @@
 <script setup lang="ts">
-import {ref} from "vue";
+import { reactive, ref } from "vue";
 import axios from "axios";
+import type { UploadFile } from 'element-plus'
+import { ElMessage, ElMessageBox } from 'element-plus'
+import WriteUpload from '@/components/WriteUpload.vue'
+import WriteBody from '@/components/WriteBody.vue'
 
-const email = ref("")
-const password = ref("")
+interface Post {
+    title: string,
+    content: string,
+}
+
+const post: Post = reactive({
+    title: "",
+    content: "",
+})
 
 const write = function() {
     axios.post("/api/auth/signup", {
-        name: "lee",
-        email: email.value,
-        password: password.value
+        
     })
+}
+
+const image = ref();
+const attachImage = function(file: UploadFile) {
+    image.value = file;
+}
+
+const postAttachImage = function() {
+    image.value = null;
+    console.log(image);
+}
+
+const updateEditor = function(value: string) {
+    post.content = value;
+    console.log(post.content);
 }
 
 </script>
 
 <template>
-    <div>
-        <el-input v-model="email" placeholder="제목을 입력해주세요" />
-    </div>
-    <div class="mt-2">
-        <el-input v-model="password" type="textarea" rows="15" />
-    </div>
-    <div class="mt-2">
-        <el-button type="primary" @click="write()">글 작성완료</el-button>
+    <div class="container">
+        <h2>글 작성 페이지</h2>
+        <el-form label-width="120px">
+            <el-form-item label="제목">
+                <el-input v-model="post.title" placeholder="제목을 입력해주세요" />
+            </el-form-item>
+            
+            <div id="write-body">
+                <!-- <el-input v-model="post.content" placeholder="내용을 입력해주세요" type="textarea" rows="15" /> -->
+                <WriteBody :image="image" @post-attach-image="postAttachImage" @update-editor="updateEditor" />
+            </div>
+
+            <div class="line" />
+
+            <el-form-item>
+                <WriteUpload @attach-image="attachImage" />
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="write()">글 작성완료</el-button>
+                <el-button type="info" @click="write()">취소</el-button>
+            </el-form-item>
+        </el-form>
     </div>
 </template>
 
-<style>
+<style scoped>
+#write-body {
+    margin: 50px 0 0 120px;
+    border: solid var(--el-border-color) 1px;
+    padding: 5px;
+}
+
+.line {
+    width: 100%;
+    height: 0;
+    margin: 30px 0 30px 120px;
+    border-top: 1px solid var(--el-border-color);
+}
 </style>
