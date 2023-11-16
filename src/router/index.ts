6a3905f4b3249a33428e607bb4876e3cf96b2232
hgleeee <94894328+hgleeee@users.noreply.github.com>
2023-11-16 21:50:25 +0900

    [#3] feat: 회원가입 페이지 구성

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import WriteView from '@/views/WriteView.vue'
 import PostPageView from '@/views/PostPageView.vue'
+import SignupView from '@/views/SignupView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/posts',
       name: 'post',
       component: PostPageView
+    },
+    {
+      path: '/join',
+      name: 'signup',
+      component: SignupView
     }
     // {
     //   path: '/about',
